6b97e2733bd57e4c4526ad03828b32f7a790d3eb
Jon Areas <areasjx@gmail.com>
2023-05-16 09:42:23 -0600

    Add `pl-api-gateway` module

---
+++ pl-api-gateway/src/test/java/com/polylab/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polylab.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
