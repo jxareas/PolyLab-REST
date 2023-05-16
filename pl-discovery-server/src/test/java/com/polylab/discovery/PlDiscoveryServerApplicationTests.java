8c7065b352966c3b7b4726f80d45375a2835ea62
Jon Areas <areasjx@gmail.com>
2023-05-16 09:26:13 -0600

    Add `pl-discovery-server` module

---
+++ pl-discovery-server/src/test/java/com/polylab/discovery/PlDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polylab.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
